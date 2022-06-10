0c44a316b444f6bd276c28a8deedf898161651a8
Marcelo Henrique <marcelohcb@msn.com>
2022-06-10 19:34:20 -0300

    upgrade

---
+++ src/data/hooks/pages/pets/Relatorio.ts
@@ -0,0 +1,8 @@
+import { Pet } from "../../../@types/Pet";
+
+export interface Relatorio{
+    id: Number;
+    email: string;
+    valor: string;
+    pet:Pet;
+}
---
+++ src/ui/components/CabecalhoAdmin/CabecalhoAdmin.style.tsx
@@ -0,0 +1,23 @@
+import { styled } from "@mui/material";
+
+
+export const CabecalhoContainer = styled("header")`    
+height:115PX;
+background-color: #f6f6f6;
+padding: ${({theme}) => theme.spacing(2)};
+    div {
+        height:100%;
+        max-width:970px;
+        margin: 0 auto;
+        display:flex;
+        justify-content:space-between;
+        align-items:center;
+        gap:${({theme}) => theme.spacing(2)};
+    }
+`;
+export const Logo = styled("img")`
+    width: 125px;
+`;
+
+export const LinksContainer = styled("nav")``;
+
